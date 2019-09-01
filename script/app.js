e3206d6422567471272ec536a0c6d7708ae6e9b0
Glaston D'souza <glastondsouza97@gmail.com>
2019-09-01 19:21:54 +0530

    Final Commit with animated balls

---
+++ script/app.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#7bd360",
+    "#720606"
+  ];
+
+  //   Adding Sounds
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  // Create function that makes Bubbles
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
